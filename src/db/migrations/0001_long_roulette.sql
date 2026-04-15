d260a2bff464b4fb0f0625125465ada9df4ff8e1
adnope <anhduyxh7@gmail.com>
2026-04-15 14:01:57 +0700

    refactor: update migration files

--- src/db/migrations/0001_long_roulette.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "messages" DROP CONSTRAINT "messages_content_check";--> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "replied_message_id" uuid;--> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "is_deleted" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "messages" ADD CONSTRAINT "messages_replied_message_id_fkey" FOREIGN KEY ("replied_message_id") REFERENCES "public"."messages"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "messages" ADD CONSTRAINT "messages_content_check" CHECK (is_deleted = true OR length(TRIM(BOTH FROM content)) > 0);
